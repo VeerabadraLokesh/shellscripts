06085af2fb5a106265b78eeba1c808b92bd60642
Veerabadra Lokesh <lokesh.pvl@gmail.com>
2020-07-25 18:47:50 +0530

    chagnes

--- .bash_aliases
+++ .bash_aliases
@@ -3,3 +3,18 @@
 alias l='ls -ltr'
 alias ld='ls -d $PWD/*'
 
+
+
+
+alias lsm='du --threshold=1M -sh ./*'
+alias lsg='du --threshold=1G -sh ./*'
+alias lsd='du -sh ./*'
+
+
+#alias ffnp='firefox -p NewProfile'
+
+
+alias syncdatetime='sudo ntpdate 1.ro.pool.ntp.org'
+
+alias raliases='source ~/.bash_aliases'
+alias r='source ~/.bash_aliases'
--- allCommands.sh
+++ allCommands.sh
@@ -15,3 +15,25 @@ tree > home_utf8.tree
 
 tree | sed 's/+/\+/g; s/-/-/g; s/+/\\/g'
 
+
+
+
+# recover files
+exit 0
+
+ext4magic /dev/sda1 -r -d /run/media/$USER/ext4/ubuntuDrive/
+
+
+extundelete
+
+extundelete /dev/sda1 --restore-all
+
+ext4magic /dev/sda1 -r -d /run/media/$USER/ext4/ubuntuDrive/
+
+
+
+ext4magic /dev/sda2 -f "/path/" -r -d /run/media/$USER/freeSpace/recoverData/
+
+
+sudo ext4magic /dev/sda2 -f "/path/" -r -d /run/media/$USER/freeSpace/recoverData/
+
